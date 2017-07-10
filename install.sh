98f5d940f963aafd676482aa3404bd7afd1e1cd4
Warwick Booth <warwick@lsdev.biz>
2017-07-10 15:43:59 +0200

    Added in the function to grab the IP, then validate it using the built in PHP functions. It also detects the v4 or v6 protocol

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Output the welcome message
+
+echo ========================================
+echo Setup Wizard
+echo ========================================
+echo You will be required to enter values for the parameters below.
+read varname
+echo It\'s nice to meet you $varname
